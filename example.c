0d0ffc48545418f9e38b6d27be909ed926f013bf
Matjaž <dev@matjaz.it>
2019-05-08 13:17:32 +0200

    Fixes in the example file for floating point approximate equality
    - Improved tests for string equality
    - Added tests for memory equality
    - Added printing of which failing lines should appear in the output

--- example.c
+++ example.c
@@ -32,17 +32,20 @@ static void test_case_flags(void)
 
 static void test_case_floats_and_doubles(void)
 {
-    const float radius = 4.0f;
+    const float radius = 0.4f;
     const float pi = 3.14159f;
     const float circle_area = pi * radius * radius;
-    atto_fapprox(circle_area, 39.478417f);
+    atto_assert(circle_area == 0.502654433f);  /* Exact value */
+    atto_fapprox(circle_area, 0.5026544f);     /* Approximate value */
 
-    const double d_radius = 4.0;
+    const double d_radius = 0.4;
     const double d_pi = 3.141592653589793;
     const double d_circle_area = d_pi * d_radius * d_radius;
-    atto_dapprox(d_circle_area, 39.4784176044);
+    atto_assert(d_circle_area == 0.5026548245743669); /* Exact value */
+    atto_dapprox(d_circle_area, 0.50265482457);       /* Approximate value */
 
-    atto_fapprox(1.000001f, 1.0f); /* This will fail. */
+    atto_fapprox(1.000001f, 1.0f);
+    atto_fapprox(1.0001f, 1.0f); /* This will fail. */
 }
 
 static void test_case_strings(void)
@@ -51,7 +54,21 @@ static void test_case_strings(void)
     strcat(result, "hello");
     strcat(result, "world");
     atto_streq("helloworld", result, 11);
+    atto_streq("helloworld", result, 20);
+    atto_streq("helloworld", result, 6);
     atto_streq("HELLOworld", result, 11); /* This will fail. */
+}
+
+static void test_case_arrays(void)
+{
+    const char a1[4] = {1, 2, 3, 4};
+    const char a2[4] = {1, 2, 3, 4};
+    const char a3[4] = {1, 2, 120, 120};
+
+    atto_memeq(a1, a2, 2);
+    atto_memeq(a1, a2, 4);
+    atto_memeq(a1, a3, 2);
+    atto_memeq(a1, a3, 4);  /* This will fail. */
 }
 
 static void test_case_forced_fail(void)
@@ -69,10 +86,14 @@ static void test_case_forced_fail(void)
 
 int main(void)
 {
+    printf("Failures should appear on lines: %d, %d, %d, %d, %d, %d\n",
+           18, 30, 48, 59, 71, 82);
     test_case_ints();
     test_case_flags();
     test_case_floats_and_doubles();
     test_case_strings();
+    test_case_arrays();
     test_case_forced_fail();
+    printf("The exit code of the program should be 1");
     return atto_at_least_one_fail;  /* Returns 1 as some tests failed. */
 }
