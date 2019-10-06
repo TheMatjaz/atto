630b468e18672d01a4fb17065608bd61648f0f43
Matjaž <dev@matjaz.it>
2019-10-06 11:19:52 +0200

    Add doxygen headers to all new macros, fixes/improv. in some old headers

--- atto.h
+++ atto.h
@@ -58,7 +58,7 @@ static char atto_at_least_one_fail = 0;
 
 /**
  * Absolute tolerance when comparing two single-precision floating point
- * value.
+ * value for approximate-equality
  *
  * If the difference between the two float values is bigger than this
  * tolerance, the assertion fails.
@@ -67,7 +67,7 @@ static char atto_at_least_one_fail = 0;
 
 /**
  * Absolute tolerance when comparing two double-precision floating point
- * value.
+ * value for approximate-equality.
  *
  * If the difference between the two double values is bigger than this
  * tolerance, the assertion fails.
@@ -77,13 +77,18 @@ static char atto_at_least_one_fail = 0;
 /**
  * Verifies if the given boolean expression is true.
  *
- * Otherwise stops the test case and reports on standard output.
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * It's the most generic macro offered by Atto. If you need something more
+ * specific, try using the other macros instead.
  *
  * The `do-while(0)` construct allows to write multi-line macros.
  *
  * Example:
  * ```
  * atto_assert(1);      // Passes
+ * atto_assert(0);      // Fails
  * atto_assert(3 < 1);  // Fails
  * ```
  */
@@ -96,69 +101,201 @@ static char atto_at_least_one_fail = 0;
     } \
 } while (0)
 
+/**
+ * Verifies if the given boolean expression is false.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * Example:
+ * ```
+ * atto_false(1);      // Fails
+ * atto_false(0);      // Passes
+ * ```
+ */
 #define atto_false(x) atto_assert(!(x))
+
+/**
+ * Verifies if the two arguments are exactly equal.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * It is recommended to use this macro instead of calling `atto_assert(a == b)`
+ * to avoid mistyping the equality comparison operator `==` into the assignment
+ * operator `=`.
+ *
+ * Example:
+ * ```
+ * atto_eq(12, 12);      // Passes
+ * atto_eq(12.0f, 12U);  // Passes due to implicit conversion of 12U to 12.0f
+ * atto_eq(100, 1);      // Fails
+ * ```
+ */
 #define atto_eq(a, b) atto_assert((a) == (b))
+
+/**
+ * Verifies if the two arguments are not equal.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * Example:
+ * ```
+ * atto_neq(12, 12);      // Fails
+ * atto_neq(12.0f, 12U);  // Fails
+ * atto_neq(100, 1);      // Passes
+ * ```
+ */
 #define atto_neq(a, b) atto_assert((a) != (b))
+
+/**
+ * Verifies if the first argument is strictly Greater Than the second.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * Example:
+ * ```
+ * atto_gt(1, 10);     // Fails
+ * atto_gt(10, 10);    // Fails
+ * atto_gt(100, 10);   // Passes
+ * ```
+ */
 #define atto_gt(a, b) atto_assert((a) > (b))
+
+/**
+ * Verifies if the first argument is Greater or Equal to the second.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * Example:
+ * ```
+ * atto_ge(1, 10);     // Fails
+ * atto_ge(10, 10);    // Passes
+ * atto_ge(100, 10);   // Passes
+ * ```
+ */
 #define atto_ge(a, b) atto_assert((a) >= (b))
+
+/**
+ * Verifies if the first argument is strictly Less Than the second.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * Example:
+ * ```
+ * atto_lt(1, 10);     // Passes
+ * atto_lt(10, 10);    // Fails
+ * atto_lt(100, 10);   // Fails
+ * ```
+ */
 #define atto_lt(a, b) atto_assert((a) < (b))
+
+/**
+ * Verifies if the first argument is Less or Equal to the second.
+ *
+ * Otherwise stops the test case immediately and reports the failing file,
+ * function and line number on standard output.
+ *
+ * Example:
+ * ```
+ * atto_le(1, 10);     // Passes
+ * atto_le(10, 10);    // Fails
+ * atto_le(100, 10);   // Fails
+ * ```
+ */
 #define atto_le(a, b) atto_assert((a) <= (b))
 
 /**
- * Verifies if two single-precision floating point values are closer than
- * the float absolute tolerance #ATTO_FLOAT_EQ_ABSTOL.
+ * Verifies if two single-precision floating point values are within a given
+ * absolute tolerance from each other.
  *
  * Otherwise stops the test case and reports on standard output.
  *
  * Example:
  * ```
- * atto_assert(1.0f == 1.00000001f);  // Fails
- * atto_fapprox(1.0f, 1.00000001f);   // Passes
- * atto_fapprox(1.0f, 2.0f);          // Fails
+ * atto_fdelta(1.0f, 1.00000001f, 0.1f);  // Passes
+ * atto_fdelta(1.0f, 1.1f, 0.1f);         // Passes
+ * atto_fdelta(1.0f, 2.0f, 0.1f);         // Fails
  * ```
  */
-#define atto_fapprox(a, b) atto_assert(fabsf((a)-(b)) <= ATTO_FLOAT_EQ_ABSTOL)
 #define atto_fdelta(a, b, delta) atto_assert(fabsf((a) - (b)) <= fabsf(delta))
 
 /**
- * Verifies if two double-precision floating point values are closer than
- * the double absolute tolerance #ATTO_DOUBLE_EQ_ABSTOL.
+ * Verifies if two single-precision floating point values are within a fixed
+ * absolute tolerance #ATTO_FLOAT_EQ_ABSTOL from each other.
+ *
+ * Useful to check for almost-equality but ignoring minor rounding errors.
  *
  * Otherwise stops the test case and reports on standard output.
  *
  * Example:
  * ```
- * atto_assert(1.0 == 1.00000001);  // Fails
- * atto_dapprox(1.0, 1.00000001);   // Passes
- * atto_dapprox(1.0, 2.0);          // Fails
+ * atto_fapprox(1.0f, 1.00000001f);  // Passes
+ * atto_fapprox(1.0f, 1.1);          // Fails
  * ```
  */
-#define atto_dapprox(a, b) atto_assert(fabs((a)-(b)) <= ATTO_DOUBLE_EQ_ABSTOL)
+#define atto_fapprox(a, b) atto_fdelta((a), (b), ATTO_FLOAT_EQ_ABSTOL)
+
+/**
+ * Verifies if two double-precision floating point values are within a given
+ * absolute tolerance from each other.
+ *
+ * Otherwise stops the test case and reports on standard output.
+ *
+ * Example:
+ * ```
+ * atto_ddelta(1.0, 1.00000001, 0.1);  // Passes
+ * atto_ddelta(1.0, 1.1, 0.1);         // Passes
+ * atto_ddelta(1.0, 2.0, 0.1);         // Fails
+ * ```
+ */
 #define atto_ddelta(a, b, delta) atto_assert(fabs((a) - (b)) <= fabs(delta))
 
 /**
- * Verifies if the bits of the value specified as a bit mask is set to 1.
+ * Verifies if two double-precision floating point values are within a fixed
+ * absolute tolerance #ATTO_DOUBLE_EQ_ABSTOL from each other.
+ *
+ * Useful to check for almost-equality but ignoring minor rounding errors.
  *
  * Otherwise stops the test case and reports on standard output.
  *
  * Example:
  * ```
- * atto_flag(7, 0x04);  // Passes
- * atto_flag(7, 0x06);  // Passes
- * atto_flag(7, 0xF0);  // Fails
+ * atto_dapprox(1.0, 1.0000000001);  // Passes
+ * atto_dapprox(1.0, 1.1);           // Fails
+ * ```
+ */
+#define atto_dapprox(a, b) atto_ddelta((a), (b), ATTO_DOUBLE_EQ_ABSTOL)
+
+/**
+ * Verifies if the bits of the value specified by a bit mask are set to 1.
+ *
+ * Otherwise stops the test case and reports on standard output.
+ *
+ * Example:
+ * ```
+ * atto_flag(0x07, 1 << 1);  // Passes
+ * atto_flag(0x07, 0x04);    // Passes
+ * atto_flag(0x07, 0x06);    // Passes
+ * atto_flag(0x07, 0xF0);    // Fails
  * ```
  */
 #define atto_flag(value, mask) atto_assert(((value) & (mask)))
 
 /**
- * Verifies if the bits of the value specified as a bit mask is set to 0.
+ * Verifies if the bits of the value specified by a bit mask are set to 0.
  *
  * Otherwise stops the test case and reports on standard output.
  *
  * Example:
  * ```
- * atto_flag(7, 0x04);  // Fails
- * atto_flag(7, 0xF8);  // Passes
+ * atto_noflag(0x07, 1 << 5);  // Passes
+ * atto_noflag(0x07, 0xF8);    // Passes
+ * atto_noflag(0x07, 0x04);    // Fails
  * ```
 */
 #define atto_noflag(value, mask) atto_assert(((value) & (mask)) == 0)
@@ -173,8 +310,8 @@ static char atto_at_least_one_fail = 0;
  * ```
  * atto_streq("abcd", "abcd", 2);    // Passes
  * atto_streq("abcd", "abcd", 4);    // Passes
- * atto_streq("abcd", "abcd", 100);  // Passes
- * atto_streq("abcd", "ABCD", 4);    // Fails
+ * atto_streq("abcd", "abcd", 100);  // Passes, reached null terminator
+ * atto_streq("abcd", "ABCD", 4);    // Fails, different casing
  * ```
  */
 #define atto_streq(a, b, maxlen) atto_assert(strncmp((a), (b), (maxlen)) == 0)
