7162184845ce719587e7d1da2bc1aa2fbcbd3466
Matjaž <dev@matjaz.it>
2020-02-05 20:22:57 +0100

    Update docs to Atto v1.2.0

--- menudata.js
+++ menudata.js
@@ -21,11 +21,4 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Related Pages",url:"pages.html"},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"},
-{text:"Globals",url:"globals.html",children:[
-{text:"All",url:"globals.html"},
-{text:"Variables",url:"globals_vars.html"},
-{text:"Macros",url:"globals_defs.html"}]}]}]}
+{text:"Main Page",url:"index.html"}]}
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,27 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abc",
-  1: "a",
-  2: "a",
-  3: "a",
-  4: "abc"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "files",
-  2: "variables",
-  3: "defines",
-  4: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Files",
-  2: "Variables",
-  3: "Macros",
-  4: "Pages"
 };
 
